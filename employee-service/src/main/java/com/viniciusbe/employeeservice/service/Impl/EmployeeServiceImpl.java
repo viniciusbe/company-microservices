49e379caf324ad65a418f797d5ae67e200585aa9
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-06-05 16:15:40 -0300

    feat: handle user not found exception

--- employee-service/src/main/java/com/viniciusbe/employeeservice/service/Impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/viniciusbe/employeeservice/service/Impl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.viniciusbe.employeeservice.service.Impl;
 
 import com.viniciusbe.employeeservice.dto.EmployeeDto;
 import com.viniciusbe.employeeservice.entity.Employee;
+import com.viniciusbe.employeeservice.exception.ResourceNotFoundException;
 import com.viniciusbe.employeeservice.mapper.EmployeeMapper;
 import com.viniciusbe.employeeservice.repository.EmployeeRepository;
 import com.viniciusbe.employeeservice.service.EmployeeService;
@@ -27,7 +28,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeDto getEmployeeByID(Long employeeId) {
-        Employee employee = employeeRepository.findById(employeeId).get();
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee", "id", employeeId)
+        );
         EmployeeDto employeeDto = employeeMapper.mapToEmployeeDto(employee);
 
         return employeeDto;
