5af81c28884f0ef155c5b915ebd3be296cafa800
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-06-06 21:53:32 -0300

    feat: add eureka configuration on department and employee services

--- employee-service/src/main/java/com/viniciusbe/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/com/viniciusbe/employeeservice/service/APIClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080/api/departments/", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE",path = "/api/departments/")
 public interface APIClient {
     @GetMapping("{department-code}")
     DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode);
